bc138694b1400093191b00c13947f3e1b2063461
Steven Winston <swinston100@hotmail.com>
2016-11-16 09:54:41 +0200

    added functionality to add and clear items

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
-// an array with all of our cart items
-var cart = [];
+function ShoppingCartApp() {
 
-var updateCart = function () {
-  // TODO: finish
+    // an array with all of our cart items
+    var cart = {
+        items: [],
+        total: 0
+    };
+
+    var renderCart = function() {
+        console.log("rendered");
+        cart.total = cart.items.reduce(function(prev, next) {
+            return prev + next.price
+        }, 0)
+
+        $(".shopping-cart").empty()
+        var source = $('#shopping-cart-template').html();
+        var template = Handlebars.compile(source);
+        var newHTML = template(cart);
+        $(".shopping-cart").append(newHTML);
+    }
+
+
+    var addItem = function(item) {
+        cart.items.push(item)
+        renderCart();
+    }
+
+    var clearCart = function() {
+        cart = {
+            items: [],
+            total: 0
+        };
+    }
+
+    return {
+        renderCart: renderCart,
+        addItem: addItem,
+        clearCart: clearCart
+    }
 }
 
 
-var addItem = function (item) {
-  // TODO: finish
-}
+var app = ShoppingCartApp()
 
-var clearCart = function () {
-  // TODO: finish
-}
-
-$('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
+$('.view-cart').on('click', function() {
+    $(".shopping-cart").toggleClass('show')
 });
 
-$('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
-  addItem(item);
-  updateCart();
+$('.add-to-cart').on('click', function() {
+    var item = $(this).closest(".item").data()
+    app.addItem(item);
+
 });
 
-$('.clear-cart').on('click', function () {
-  clearCart();
+$('.shopping-cart').on('click', $('.clear-cart'), function() {
+    app.clearCart();
+    $(".shopping-cart").removeClass('show')
+    $(".shopping-cart").empty()
 });
 
 // update the cart as soon as the page loads!
-updateCart();
+app.renderCart();
