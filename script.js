acc7fd491b68372cdd9eb5bdb4a31964bd37e975
Steven Winston <swinston100@hotmail.com>
2016-11-17 17:59:41 +0200

    local storage, header, started admin

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 function ShoppingCartApp() {
 
-    // an array with all of our cart items
-    var cart = {
-        items: [],
-        total: 0
-    };
+    var cart = {};
+
+    var store = {};
+
+    var CART_STORE = 'shopping_cart';
+    var ITEM_STORE = 'item_store';
+
+    var getFromLocalStorage = function() {
+        //if there is no cart then set it up!
+        cart = JSON.parse(localStorage.getItem(CART_STORE) || '{"items": [],"total": 0}');
+        store = JSON.parse(localStorage.getItem(ITEM_STORE) || '{"id":6,"items":[{"name":"Glass","price":68,"image":"http://ecx.images-amazon.com/images/I/31AOX24ATKL.jpg","id":0},{"name":"Pencils","price":3,"image":"http://ecx.images-amazon.com/images/I/51YFEe%2BCYbL.jpg","id":1},{"name":"Kinfolk","price":21,"image":"http://ecx.images-amazon.com/images/I/41m0VhULItL.jpg","id":2},{"name":"Book","price":25,"image":"http://ecx.images-amazon.com/images/I/41uyfSEwr0L.jpg","id":3},{"name":"Pipe","price":124,"image":"http://ecx.images-amazon.com/images/I/41TvbxcZpZL.jpg","id":4},{"name":"Stool","price":92,"image":"http://ecx.images-amazon.com/images/I/41NZO5GovmL.jpg","id":5}]}');
+        //console.log(store);
+    }
+    var _saveToLocalStorage = function() {
+        localStorage.setItem(CART_STORE, JSON.stringify(cart));
+        localStorage.setItem(ITEM_STORE, JSON.stringify(store));
+    }
+
+    var renderItems = function() {
+        $("#store-container").empty()
+        store.items.forEach(function(item) {
+            var source = $('#item-template').html();
+            var template = Handlebars.compile(source);
+            var newHTML = template(item);
+            //add the html to the  container
+            $("#store-container").append(newHTML);
+        })
+    }
 
     var renderCart = function() {
-        cart.total = cart.items.reduce(function(prev, next) {
-            return prev + next.price * next.quantity
-        }, 0)
-
         $(".shopping-cart").empty()
         var source = $('#shopping-cart-template').html();
         var template = Handlebars.compile(source);
@@ -20,29 +39,38 @@ function ShoppingCartApp() {
 
 
     var addItem = function(item) {
+        var matchedIndex = cart.items.findIndex(function(element) {
+            return element.id === item.id;
+        });
 
-        var index = cart.items.indexOf(item)
-            //if the item is not in the cart then add it and set quantity to one
-            //else increase quantity
-        if (index === -1) {
+        if (matchedIndex != -1) {
+            cart.items[matchedIndex].quantity++
+        } else {
             item.quantity = 1;
             cart.items.push(item);
-        } else {
-            cart.items[index].quantity++
         }
-        console.log(cart.items);
+
+        //update the total
+        cart.total = cart.items.reduce(function(prev, next) {
+            return prev + next.price * next.quantity
+        }, 0)
+
+        _saveToLocalStorage();
         renderCart();
+
     }
 
-    var removeItem = function(item) {
+    var removeItem = function(btn) {
+        var itemID = $(btn).closest('.item').data().id;
         cart.items.forEach(function(cartitem) {
-            if (item === cartitem.name && cartitem.quantity > 1) {
+            if (itemID === cartitem.id && cartitem.quantity > 1) {
                 cartitem.quantity--;
-            } else if (item === cartitem.name && cartitem.quantity === 1) {
+            } else if (itemID === cartitem.id && cartitem.quantity === 1) {
                 //remove the item from the array completely
                 cart.items.splice(cart.items.indexOf(cartitem), 1);
             }
         })
+        _saveToLocalStorage();
         renderCart();
     }
 
@@ -51,48 +79,84 @@ function ShoppingCartApp() {
             items: [],
             total: 0
         };
+        _saveToLocalStorage();
         renderCart();
     }
 
     return {
+        renderItems: renderItems,
         renderCart: renderCart,
         addItem: addItem,
         clearCart: clearCart,
-        removeItem: removeItem
+        removeItem: removeItem,
+        getFromLocalStorage: getFromLocalStorage
     }
 }
 
+$('#header').load('./header.html');
 
-var app = ShoppingCartApp()
+var app = ShoppingCartApp();
 
-$('.view-cart').on('click', function() {
-    $(".shopping-cart").toggleClass('show')
-});
-
-$('.add-to-cart').on('click', function() {
-    var item = $(this).closest(".item").data()
-    app.addItem(item);
-
-});
-
-$('.shopping-cart').on('click', '.clear-cart', function() {
-    app.clearCart();
-    $(".shopping-cart").removeClass('show')
-});
-
-$('.shopping-cart').on('click', '.remove', function() {
-    app.removeItem($(this).closest('.item').data().item)
-});
-
+//turns off shopping cart when anywhere on the window is clicked!
 $(window).click(function() {
     if ($(".shopping-cart").hasClass('show')) {
         $(".shopping-cart").removeClass('show');
     }
 });
 
-$(".shopping-cart").click(function(event) {
-    event.stopPropagation();
+$(document).ready(function() {
+
+    app.getFromLocalStorage();
+
+    var location = this.location.pathname
+    if (location.indexOf('index') != -1) {
+        app.renderItems();
+        setupAddItemListener()
+    } else if (location.indexOf('admin') != -1) {
+        //setup parsley for admin form
+        var form = $('#item-form').parsley();
+    }
+    // update the cart as soon as the index page loads!
+    app.renderCart();
+    setupCartListeners()
+    highlightNavBar(location)
 });
 
-// update the cart as soon as the page loads!
-app.renderCart();
+function highlightNavBar(location) {
+    var navLinks = $('nav li a').each(function(i, link) {
+        var href = $(link).attr('href');
+        if (location.indexOf(href) != -1) {
+            $(link).closest('li').addClass('active')
+        }
+    })
+}
+
+function setupAddItemListener() {
+    $('.add-to-cart').on('click', function(e) {
+        e.stopPropagation();
+        var item = $(this).closest(".item").data();
+        //item = Object.create($(this).closest(".item").data())
+        //debugger;
+        app.addItem(item);
+        $(".shopping-cart").addClass('show')
+    });
+}
+
+function setupCartListeners() {
+    $('.view-cart').on('click', function() {
+        $(".shopping-cart").toggleClass('show')
+    });
+
+    $('.shopping-cart').on('click', '.clear-cart', function() {
+        app.clearCart();
+        $(".shopping-cart").removeClass('show')
+    });
+
+    $('.shopping-cart').on('click', '.remove', function() {
+        app.removeItem(this)
+    });
+
+    $(".shopping-cart").click(function(event) {
+        event.stopPropagation();
+    });
+}
